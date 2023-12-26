3d34a7697a6f546a24a996d4ff4b79ab1a70a8ef
Cirrus-8691 <diaz.frederic@gmail.com>
2023-12-26 21:41:07 +0100

    TestMongoDb

--- services/Tests/TestMongoDb.ts
+++ services/Tests/TestMongoDb.ts
@@ -4,82 +4,86 @@ import MongoDatabase from "../serviceMongodb/Model/MongoDatabase";
 import MongoDbConnect from "../serviceMongodb/Model/MongoDbConnect";
 
 export const TestLocalMongoDbUser = "usr";
-export const TestLocalMongoDbName = "Histo";
-export const TestLocalMongoDbUrl = `mongodb://${TestLocalMongoDbUser}:Flin*123@192.168.232.133:27017/${TestLocalMongoDbName}`;
+export const TestLocalMongoDbName = "fred";
+export const TestLocalMongoDbUrl = `mongodb://${TestLocalMongoDbUser}:Pwd*175@192.168.0.24:27017/${TestLocalMongoDbName}`;
 
 export default async function TestMongoDb() {
     const db = new MongoDatabase();
+    try {
 
-    const dbConnect = new MongoDbConnect(TestLocalMongoDbUrl);
-    db.connect(dbConnect);
-    await db.test();
+        const dbConnect = new MongoDbConnect(TestLocalMongoDbUrl);
+        db.connect(dbConnect);
+        await db.test();
 
-    const collections = await db.getRepositories();
-    assert.isTrue(collections.length > 0);
+        const collections = await db.getRepositories();
+        assert.isTrue(collections.length > 0, "collections.length > 0");
 
-    let documents = await db.findOnRepository(
-        {
+        let documents = await db.findOnRepository(
+            {
+                collection: collections[0],
+                what: "{}",
+                skip: "0",
+                limit: "10"
+            });
+        assert.isDefined(documents, "documents Defined");
+
+        const docWorld = { Hello: "World" };
+        const findWorld = JSON.stringify(docWorld);
+        let ok = await db.insertEntity(
+            { collection: collections[0] },
+            docWorld
+        );
+        assert.isTrue(ok, "insertEntity OK");
+
+        documents = await db.findOnRepository({
             collection: collections[0],
-            what: "{}",
+            what: findWorld,
             skip: "0",
-            limit: "10"
+            limit: "1"
         });
-    assert.isDefined(documents);
+        assert.isNotEmpty(documents, "insertEntity findOnRepository ");
+        assert.equal(1, documents.length);
+        assert.equal(docWorld.Hello, documents[0].Hello);
 
-    const docWorld = { Hello: "World" };
-    const findWorld = JSON.stringify(docWorld);
-    let ok = await db.insertEntity(
-        { collection: collections[0] },
-        { document: docWorld }
-    );
-    assert.isTrue(ok);
+        const docXyz = {
+            _id: documents[0]._id,
+            Hello: "Xyz"
+        };
+        const findXyz = JSON.stringify(docXyz);
+        ok = await db.updateEntity(
+            {
+                collection: collections[0]
+            },
+            docXyz);
+        assert.isTrue(ok, "updateEntity Ok");
 
-    documents = await db.findOnRepository({
-        collection: collections[0],
-        what: findWorld,
-        skip: "0",
-        limit: "1"
-    });
-    assert.isNotEmpty(documents);
-    assert.equal(1, documents.length);
-    assert.equal(docWorld.Hello, documents[0].Hello);
+        documents = await db.findOnRepository({
+            collection: collections[0],
+            what: JSON.stringify({ Hello: docXyz.Hello }),
+            skip: "0",
+            limit: "1"
+        });
+        assert.isNotEmpty(documents, "updateEntity findOnRepository ");
+        assert.equal(1, documents.length);
+        assert.equal(docXyz.Hello, documents[0].Hello);
 
-    const docXyz= { Hello: "Xyz" };
-    const findXyz = JSON.stringify(docXyz);
-    ok = await db.updateEntity(
-        {
+        ok = await db.deleteEntity({
             collection: collections[0],
             _id: documents[0]._id
-        },
-        {
-            document: docXyz
         });
-    assert.isTrue(ok);
+        assert.isTrue(ok, "deleteEntity OK");
 
-    documents = await db.findOnRepository({
-        collection: collections[0],
-        what: findXyz,
-        skip: "0",
-        limit: "1"
-    });
-    assert.isNotEmpty(documents);
-    assert.equal(1, documents.length);
-    assert.equal(docXyz.Hello, documents[0].Hello);
+        documents = await db.findOnRepository({
+            collection: collections[0],
+            what: findXyz,
+            skip: "0",
+            limit: "1"
+        });
+        assert.isEmpty(documents, "deleteEntity findOnRepository ");
+        assert.equal(0, documents.length);
 
-    ok = await db.deleteEntity({
-        collection: collections[0],
-        _id: documents[0]._id
-    });
-    assert.isTrue(ok);
-
-    documents = await db.findOnRepository({
-        collection: collections[0],
-        what: findXyz,
-        skip: "0",
-        limit: "1"
-    });
-    assert.isEmpty(documents);
-    assert.equal(0, documents.length);
-
-    db.dispose();
+    }
+    finally {
+        db.dispose();
+    }
 }
--- services/Tests/_runTests.ts
+++ services/Tests/_runTests.ts
@@ -9,7 +9,7 @@ describe("Tests", () => {
     describe("Unit tests", () => {
         it(`Test StubDb `,     TestStubDb);
         it(`Test Service `,    TestServiceMongo);
-        //it(`Test MongoDb `,    TestMongoDb);
+        it(`Test MongoDb `,    TestMongoDb);
         //it(`Test Service `,    TestServicePostgreSql);
      //  it(`Test PostgreSql `,    TestPostgreSql);
     });
