8d5a97339b559963838292cc909c39d07c8cd7fc
Cirrus-8691 <diaz.frederic@gmail.com>
2023-12-28 20:07:55 +0100

    serviceHost

--- services/src/Gateway/Controller/PostgreController.ts
+++ services/src/Gateway/Controller/PostgreController.ts
@@ -7,7 +7,7 @@ export default class PostgreController extends GenericDbController {
     public constructor(server: HttpFastifyServer) {
 
         const namespace = process.env.SERVICE_NAMESPACE;
-        const serviceHost = process.env.SERVICE_POSTGRESQL_NAME ?? "127.0.0.1:4002";
+        const serviceHost = process.env.SERVICE_POSTGRESQL_NAME ?? "127.0.0.1";
         const servicePort = process.env.SERVICE_POSTGRESQL_PORT ?? "4002";
         const serviceRoute = `http://${serviceHost}${namespace ? "." + namespace : ""}:${servicePort}${QueryController.RouteBeginning}`;
 
